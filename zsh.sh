2be41f3324062ada110e5b988437a1d3f51d3129
mist <mistwork03@gmail.com>
2025-08-31 15:56:56 +0300

    another commit

--- zsh.sh
+++ zsh.sh
@@ -19,9 +19,9 @@ cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 sed -i 's/${AGNOSTER_STATUS_ROOT_FG:=yellow}/${AGNOSTER_STATUS_ROOT_FG:=cyan}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
-sed -i 's/${AGNOSTER_GIT_CLEAN_BG:=green}/${AGNOSTER_GIT_CLEAN_BG:=black}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
+sed -i 's/${AGNOSTER_GIT_CLEAN_BG:=green}/${AGNOSTER_GIT_CLEAN_BG:=blue}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 sed -i 's/${AGNOSTER_GIT_DIRTY_FG:=black}/${AGNOSTER_GIT_DIRTY_FG:=white}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
-sed -i 's/${AGNOSTER_GIT_DIRTY_BG:=yellow}/${AGNOSTER_GIT_DIRTY_FG:=white}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
+sed -i 's/${AGNOSTER_GIT_DIRTY_BG:=yellow}/${AGNOSTER_GIT_DIRTY_FG:=black}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
 sed -i 's/${AGNOSTER_DIR_BG:=blue}/${AGNOSTER_DIR_BG:=white}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 sed -i 's/robbyrussell/agnoster/' ~/.zshrc
