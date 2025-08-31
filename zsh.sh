d64f89a2b7c7ce9b8b20326b1956df806e7eb6f9
mist <mistwork03@gmail.com>
2025-08-31 16:26:37 +0300

    another commit

--- zsh.sh
+++ zsh.sh
@@ -17,6 +17,8 @@ echo "y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh
 #clear
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
+sed -i 's/robbyrussell/agnoster/' ~/.zshrc
+
 sed -i 's/${AGNOSTER_STATUS_ROOT_FG:=yellow}/${AGNOSTER_STATUS_ROOT_FG:=cyan}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
 sed -i 's/${AGNOSTER_GIT_CLEAN_BG:=green}/${AGNOSTER_GIT_CLEAN_BG:=black}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
@@ -24,7 +26,7 @@ sed -i 's/${AGNOSTER_GIT_DIRTY_FG:=black}/${AGNOSTER_GIT_DIRTY_FG:=white}/' ~/.o
 sed -i 's/${AGNOSTER_GIT_DIRTY_BG:=yellow}/${AGNOSTER_GIT_DIRTY_FG:=black}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
 sed -i 's/${AGNOSTER_DIR_BG:=blue}/${AGNOSTER_DIR_BG:=white}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
-sed -i 's/robbyrussell/agnoster/' ~/.zshrc
+sed -i 's/${AGNOSTER_DIR_FG:=${CURRENT_FG}}/${AGNOSTER_DIR_FG:=black}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
 sed -i "s/CURRENT_FG=\${CURRENT_FG:-'black'}/CURRENT_FG=\${CURRENT_FG:-'white'}/" ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
