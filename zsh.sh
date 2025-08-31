1476e03a1188393b3bdc292c12da565be321dc78
mist <mistwork03@gmail.com>
2025-08-31 15:37:58 +0300

    another commit

--- zsh.sh
+++ zsh.sh
@@ -3,13 +3,13 @@
 echo "Script is running..."
 apt update
 apt install -y zsh curl
-clear
+#clear
 apt install powerline fonts-powerline -y
-clear
+#clear
 chsh -s $(which zsh)
 zsh --version
 echo "y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-clear
+#clear
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 sed -i 's/${AGNOSTER_STATUS_ROOT_FG:=yellow}/${AGNOSTER_STATUS_ROOT_FG:=cyan}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
