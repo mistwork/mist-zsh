650c6f22e1c02a0412546fdf20918b5298df0fcd
mist <mistwork03@gmail.com>
2025-08-31 15:32:46 +0300

    another commit

--- zsh.sh
+++ zsh.sh
@@ -21,13 +21,19 @@ sed -i 's/${AGNOSTER_GIT_DIRTY_BG:=yellow}/${AGNOSTER_GIT_DIRTY_FG:=black}/' ~/.
 sed -i 's/${AGNOSTER_DIR_BG:=blue}/${AGNOSTER_DIR_BG:=white}/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
 sed -i 's/robbyrussell/agnoster/' ~/.zshrc
 
-
+if [ -d "$HOME/.zsh-syntax-highlighting" ]; then
+    echo "Removing existing zsh-syntax-highlighting..."
+    rm -rf "$HOME/.zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
 echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+fi
 
+if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    echo "Removing existing zsh-autosuggestions..."
+    rm -rf "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.oh-my-zsh/themes/agnoster.zsh-theme
+fi
 
 exec zsh
-
 source ~/.zshrc
