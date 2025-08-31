a46e095eea3e83646b8dad3e792c0ac906a4a826
mist <mistwork03@gmail.com>
2025-08-31 13:26:50 +0300

    another commit

--- zsh.sh
+++ zsh.sh
@@ -7,7 +7,7 @@ apt install powerline fonts-powerline -y
 clear
 chsh -s $(which zsh)
 zsh --version
-echo "y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 clear
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 echo "[INFO] Starting interactive shell. Type 'exit' to quit."
